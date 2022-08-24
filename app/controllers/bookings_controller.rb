77f1388536014ec9dc47cd5e2ac0246256206ef4
Fabio Schapowal <fabiosw@me.com>
2022-08-24 13:00:57 +0100

    show page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+    @place = Place.find(params[:place_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.place = @place
+    @booking.status = "pending"
+     @booking.save
+
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:startdate, :enddate)
+  end
 end
