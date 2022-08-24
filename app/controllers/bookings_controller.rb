15be5397392a2aff4054b6b71900edb7d0d322c3
Fabio Schapowal <fabiosw@me.com>
2022-08-24 15:17:21 +0100

    booking logic

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,10 @@
 class BookingsController < ApplicationController
 
+  def show
+    @booking = Booking.find(params[:id])
+    @place = Place.find(params[:place_id])
+  end
+
   def new
     @booking = Booking.new
     @place = Place.find(params[:place_id])
@@ -7,11 +12,17 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
+    @place = Place.find(params[:place_id])
     @booking.user = current_user
     @booking.place = @place
     @booking.status = "pending"
-     @booking.save
-
+    days = (@booking.enddate - @booking.startdate).to_i
+    @booking.total_price = days * @place.price_day
+    if @booking.save
+      redirect_to place_booking_path(@place, @booking) #dashboard_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_141555) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_134456) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,14 +43,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_141555) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.string "startdate"
-    t.string "enddate"
     t.string "status"
     t.float "total_price"
     t.bigint "place_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "startdate"
+    t.date "enddate"
     t.index ["place_id"], name: "index_bookings_on_place_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
