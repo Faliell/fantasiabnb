45a8c0da86991bdd03a98e00622c0d0b73e0f4f8
Fabio Schapowal <fabiosw@me.com>
2022-08-25 17:45:24 +0100

    date validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,9 @@
+require "time"
+
 class Booking < ApplicationRecord
   belongs_to :place
   belongs_to :user
+
+  validates :startdate, comparison: { greater_than: Date.today }
+  validates :enddate, comparison: { greater_than: :startdate }
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,6 @@ class Place < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+
+  #validates :place_type, inclusion: { in: ["Castle", "Tower", "House", "Prison", "Temple"] }
 end
