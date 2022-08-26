a9afb720e7a8321faa84780a6d74127c4a173f63
Fabio Schapowal <fabiosw@me.com>
2022-08-26 15:00:43 +0100

    validates

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,5 +3,7 @@ class Place < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
 
-  #validates :place_type, inclusion: { in: ["Castle", "Tower", "House", "Prison", "Temple"] }
+  validates :description, presence: true, length: { minimum: 2, maximum: 100 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 2, maximum: 40 }
+  validates :price_day, presence: true, comparison: { greater_than: 0 }
 end
